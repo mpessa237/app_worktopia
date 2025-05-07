19bb62c700a01440e535755dc24303d093450ad7
mpessa237 <hervempessa7@gmail.com>
2025-05-07 16:53:57 +0100

    initial commit

---
+++ src/main/java/com/example/worktopia/WorktopiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.worktopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorktopiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorktopiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/worktopia/WorktopiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.worktopia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorktopiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
